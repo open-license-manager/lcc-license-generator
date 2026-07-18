02c8ebfce620ded1e8c3a5c8ce29cd640b3b7845
gcontini <1121667+gcontini@users.noreply.github.com>
2026-07-18 14:17:55 +0200

    update build to download boost from github

---
+++ scripts/windows_download_boost.ps1
@@ -0,0 +1,41 @@
+# This script downloads boost. It must be invoked with 3 arguments
+# windows_download_boost.ps1 <boost_version> <msvc version> <architecture>
+# eg. windows_download_boost.ps1 1.91.0 14.3 64
+
+if ($args.Count -ne 3) {
+    Write-Host "Error: This script requires 3 parameters.\n windows_download_boost.ps1 <boost_version> <msvc version> <architecture>"
+    exit
+}
+[string] $version = $args[0]
+[string] $msvc_version = $args[1]
+[string] $architecture = $args[2]
+$version_und = $version.Replace('.', '_')
+$uri = "https://github.com/userdocs/boost/releases/download/boost-"+$version+"/boost_" + $version_und + "-msvc-" + $msvc_version + "-" + $architecture + ".exe"
+
+if (-not (Test-Path 'C:/local/boost/libs')) {
+	echo "Boost not cached, downloading it"
+	$maxRetries = 3
+    $retryCount = 0
+    $response = $null
+
+    do {
+        try {
+                $response = Invoke-WebRequest -Uri $uri -UseBasicParsing -OutFile boost.exe
+                # If the request is successful, exit the loop
+                break
+            } catch {
+                [int]$StatusCode = $_.Exception.Response.StatusCode
+                $errorMessage = $_.Exception.Message
+                $retryCount++
+                Write-Host "Attempt $retryCount failed: $StatusCode $errorMessage. Retrying $uri ..."
+                Start-Sleep -Seconds 2  # Wait before retrying
+            }
+        } until ($retryCount -ge $maxRetries)
+
+        if ($response) {
+            Write-Host "Boost downloaded"
+        } else {
+            Write-Host "Request failed after $maxRetries attempts."
+            exit 1
+        }
+} else { Write-Host "Boost already installed" }
